026fe79367271b9def3e2665d6c048afec7a2581
Tormod G. Hellen <tormod@mbr-targeting.com>
2020-03-05 17:35:49 +0100

    Ergonomics.

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 # Have musl-tools installed
 # rustup target add x86_64-unknown-linux-musl
 
+rm *.zip
 cargo build --release --target x86_64-unknown-linux-musl
 zip -j rust.zip ./target/x86_64-unknown-linux-musl/release/bootstrap

db0b5d386b7047d3806259ed4a72e7a46291f899
Tormod G. Hellen <tormod@mbr-targeting.com>
2020-03-06 19:39:05 +0100

    Added slack mention. The signature check does not work.

--- src/main.rs
+++ src/main.rs
@@ -68,11 +68,28 @@ struct SlackChallenge {
     r#type: String
 }
 
+#[derive(Serialize)]
+#[derive(Deserialize)]
+struct SlackMention {
+    event: SlackEvent
+}
+
+#[derive(Serialize)]
+#[derive(Deserialize)]
+struct SlackEvent {
+    r#type: String,
+    text: String,
+    user: String
+}
+
+
+// TODO: Can be made internally tagged since all slack messages seem to have "type" somewhere
 #[derive(Deserialize)]
 #[serde(untagged)]
 enum InBody {
     CustomEvent(CustomEvent),
     SlackChallenge(SlackChallenge),
+    SlackMention(SlackMention)
 }
 
 
@@ -86,7 +103,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 fn my_handler(input: ApiGatewayInput, c: Context) -> Result<ApiGatewayOutput, HandlerError> {
     debug!("We received: {:?}", input.body);
     match serde_json::from_str::<InBody>(&input.body) {
-        Ok(message) => respond(&message, &input.headers),
+        Ok(message) => respond(&message, &input.headers, &input.body),
         Err(err) => {
             error!("Couldn't parse: {}. Got: {}", input.body, err);
             bail!("We fukd");
@@ -94,10 +111,11 @@ fn my_handler(input: ApiGatewayInput, c: Context) -> Result<ApiGatewayOutput, Ha
     }
 }
 
-fn respond(m: &InBody, headers: &InHeaders) -> Result<ApiGatewayOutput, HandlerError> {
+fn respond(m: &InBody, headers: &InHeaders, raw_body: &str) -> Result<ApiGatewayOutput, HandlerError> {
     match m {
         InBody::CustomEvent(e) =>  Ok(first_name_response(e)),
-        InBody::SlackChallenge(e) => slack_challenge_response(e, &headers)
+        InBody::SlackChallenge(e) => slack_challenge_response(e, &headers, raw_body),
+        InBody::SlackMention(e) => slack_mention_response(e, &headers, raw_body)
     }
 }
 
@@ -112,23 +130,43 @@ fn first_name_response(custom_event: &CustomEvent) -> ApiGatewayOutput {
     }
 }
 
-fn slack_challenge_response(challenge: &SlackChallenge, headers: &InHeaders) -> Result<ApiGatewayOutput, HandlerError> {
+fn slack_challenge_response(challenge: &SlackChallenge, headers: &InHeaders, raw_body: &str) -> Result<ApiGatewayOutput, HandlerError> {
+    match slack_verify_signature(raw_body, headers) {
+        Err(_) => bail!("Signature did not match"),
+        Ok(_) => Ok(ApiGatewayOutput {
+            status_code: 200,
+            headers: OutHeaders {
+                x_custom_header: "my custom header value".to_string()
+            },
+            body: challenge.challenge.to_owned(),
+        })
+    }
+}
+
+fn slack_mention_response(mention: &SlackMention, headers: &InHeaders, raw_body: &str) -> Result<ApiGatewayOutput, HandlerError> {
+    match slack_verify_signature(raw_body, headers) {
+        Err(_) => bail!("Signature did not match"),
+        Ok(_) => Ok(ApiGatewayOutput {
+            status_code: 200,
+            headers: OutHeaders {
+                x_custom_header: "my custom header value".to_string()
+            },
+            body: format!("Fuck off, {}. Anyway, have you considered SMASHING with each other? ;)", mention.event.user),
+        })
+    }
+}
+
+fn slack_verify_signature(body: &str, headers: &InHeaders) -> Result<bool, HandlerError> {
     match (&headers.slack_signature, &headers.slack_timestamp) {
         (Some(their_signature), Some(timestamp)) => {
-            let our_signature_unhashed = "v0".to_owned() + &timestamp + &serde_json::to_string(&challenge).unwrap();
+            let our_signature_unhashed = "v0".to_owned() + ":" + &timestamp + ":" + body;
             let signing_secret = env::var("SLACK_SECRET").expect("SLACK_SECRET env variable not found");
             let mut mac = Hmac::<Sha256>::new_varkey(signing_secret.as_bytes())
                 .expect("HMAC can take key of any size");
             mac.input(our_signature_unhashed.as_bytes());
             match mac.verify(their_signature.as_bytes()) {
                 Err(_) => bail!("Signature did not match"),
-                Ok(_) => Ok(ApiGatewayOutput {
-                    status_code: 200,
-                    headers: OutHeaders {
-                        x_custom_header: "my custom header value".to_string()
-                    },
-                    body: challenge.challenge.to_owned(),
-                })
+                Ok(_) => Ok(true)
             }
         },
         _ => bail!("Didn't have required headers")

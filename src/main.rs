b8c2f78319031bb95f1b74a72666e5ec3665b8a5
Tormod G. Hellen <tormod@mbr-targeting.com>
2020-03-06 12:57:53 +0100

    Serializing out

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 
 use lambda_runtime::{error::HandlerError, lambda, Context};
-use log::{self, error};
+use log::{self, error, debug};
 use serde_derive::{Deserialize, Serialize};
 use simple_error::bail;
 use simple_logger;
@@ -21,6 +21,11 @@ struct ApiGatewayOutput {
 }
 
 #[derive(Serialize)]
+struct Body {
+    message: String
+}
+
+#[derive(Serialize)]
 struct Headers {
     #[serde(rename = "x-custom-header")]
     x_custom_header: String
@@ -34,12 +39,27 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn my_handler(e: serde_json::value::Value, c: Context) -> Result<ApiGatewayOutput, HandlerError> {
+    debug!("We received: {:?}", e);
 
     Ok(ApiGatewayOutput {
         status_code: 200,
         headers: Headers {
             x_custom_header: "my custom header value".to_string()
         },
-        body: format!("{{\"message\":\"Ready for some, ughhhhhhnfff...., SMASH?\"}}"),
+        body: format!("{}", ""),
     })
+}
+
+fn serialize_body(body: Body) -> String {
+    serde_json::to_string(&body).unwrap()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn serialize_body_works() {
+        assert_eq!(serialize_body(Body{message: "Ready for some, ughhhhhhnfff...., SMASH?".to_string()}), format!("{{\"message\":\"Ready for some, ughhhhhhnfff...., SMASH?\"}}"));
+    }
 }
